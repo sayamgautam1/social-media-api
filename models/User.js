edd7d0e36458df5606c68b3f9d9b1f90f80a8e4a
Sayam Gautam <sayamgautam1@gmail.com>
2022-07-11 21:02:32 +0930

    add user model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: { type: String, unique: true, required: true, trim: true },
+    email: { type: String, required: true, unique: true },
+    //TODO email validation
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Friends",
+      },
+    ],
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `friendCount` that gets and sets the user's friend count
+userSchema
+  .virtual("friendCount")
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
+
+// Initialize our User model
+const User = model("user", userSchema);
+
+module.exports = User;
