baba1cbb59e4941b85c3afaeec402e73821dc01d
Sayam Gautam <sayamgautam1@gmail.com>
2022-07-13 17:25:26 +0930

    add email input validaiton

--- models/User.js
+++ models/User.js
@@ -4,8 +4,15 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema(
   {
     username: { type: String, unique: true, required: true, trim: true },
-    email: { type: String, required: true, unique: true },
-    //TODO email validation
+    email: {
+      type: String,
+      match: [
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please add a valid email address.",
+      ],
+      required: true,
+      unique: true,
+    },
     thoughts: [
       {
         type: Schema.Types.ObjectId,
