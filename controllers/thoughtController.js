d9bc66931b0b33c21ec820f3f540276fea752fe0
Sayam Gautam <sayamgautam1@gmail.com>
2022-07-14 18:56:53 +0930

    Add route to add reaction to existing thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -77,4 +77,18 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  // add reaction to existing thoughts
+  addReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with this id!" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -6,16 +6,20 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  addReaction,
 } = require("../../controllers/thoughtController");
 
 // /api/thoughts
 router.route("/").get(getThoughts).post(createThought);
 
-// /api/users/:userId
+// /api/users/:thoughtId
 router
   .route("/:thoughtId")
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
+
+// /api/thoughts/:thoughtID/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
 
 module.exports = router;
