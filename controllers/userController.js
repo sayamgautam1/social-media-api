93092fa46e4047d4ebb409471faaeec10e15ac3d
Sayam Gautam <sayamgautam1@gmail.com>
2022-07-14 16:41:29 +0930

    add update route to update details of existing users

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,6 +22,23 @@ module.exports = {
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
+  // update existing user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No video with this id!" })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
   // delete existing user
   deleteUser(req, res) {
     User.findOneAndRemove({ _id: req.params.userId })
