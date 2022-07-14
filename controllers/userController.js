8d29eab5605349d0ff2b9834c5be16eaabca82e5
Sayam Gautam <sayamgautam1@gmail.com>
2022-07-14 19:51:06 +0930

    add route to add friends to existing user

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,4 +50,18 @@ module.exports = {
 
       .catch((err) => res.status(500).json(err));
   },
+  // add friend to the user
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this id!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
